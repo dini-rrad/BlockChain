ec0218c63486e91436e2e5241961200973ff889b
dini-rrad <77625078+dini-rrad@users.noreply.github.com>
2022-04-22 19:33:03 +0700

    SendMoneyExample.sol

--- UTS/LAB 1/SendMoneyExample.sol
+++ UTS/LAB 1/SendMoneyExample.sol
@@ -2,14 +2,23 @@
 pragma solidity ^0.8.1;
 contract SendMoneyExample {
 uint public balanceReceived;
+
 function receiveMoney() public payable {
 balanceReceived += msg.value;
 }
+
 function getBalance() public view returns(uint) {
 return address(this).balance;
 }
+
+//withdraw function
 function withdrawMoney() public {
 address payable to = payable(msg.sender);
 to.transfer(getBalance());
 }
+
+//Withdraw To Specific Account
+function withdrawMoneyTo(address payable _to) public {
+_to.transfer(getBalance());
 }
+}}
